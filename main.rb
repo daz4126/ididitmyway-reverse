76937d6233cadfb0e0f413bc54157f5457f3237f
Darren Jones <daz@ironman.(none)>
2010-01-05 13:19:43 +0000

    Using external views

--- main.rb
+++ main.rb
@@ -4,15 +4,3 @@ require 'sinatra'
 get '/' do
   erb :home
 end
-
-use_in_file_templates!
-
-__END__
-
-@@ home
-
-<h1>Reverse</h1>
-
-<p>
-Welcome to the home page of my very first Sinatra app.
-</p>
